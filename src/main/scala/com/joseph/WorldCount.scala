24f6b6b09446f0dc79e61f0c21c6f5b248471799
joseph <1079822638@qq.com>
2020-07-06 21:17:53 +0800

    first commit

---
+++ src/main/scala/com/joseph/WorldCount.scala
@@ -0,0 +1,20 @@
+package com.joseph
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WorldCount {
+  def main(args: Array[String]): Unit = {
+    val logfile = "/home/hlw/app/spark-3.0.0-preview2-bin-custom-spark/README.md"
+    val conf = new SparkConf().setAppName("WorldCount").setMaster("local[2]")
+
+    val sc = new SparkContext(conf)
+
+    val logData = sc.textFile(logfile)
+
+    val result = logData.flatMap(line=>line.split(" ")).map(line=>(line,1)).reduceByKey(_+_)
+
+    result.foreach(line=>println(line._1 + "=" + line._2))
+
+  }
+
+}
